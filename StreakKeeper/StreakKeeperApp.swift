ca23fcf20a4024a454b6c943d5de82531dd21b5c
Ali Zoha <zoha0380@gmail.com>
2026-09-12 18:20:32 +0530

    Initial Commit

---
+++ StreakKeeper/StreakKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakKeeperApp.swift
+//  StreakKeeper
+//
+//  Created by Ali Zoha on 12/09/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
